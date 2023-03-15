2bc144732c67655155fecaa74a62fdede50c0e53
test <test@qq.com>
2023-03-15 22:08:29 +0800

    release1
    	modified:   build.gradle
    	modified:   src/main/java/com/example/cms9cc/template/PlayController.java
    	new file:   src/main/java/com/example/cms9cc/template/service/PlayService.java
    	modified:   src/main/resources/templates/ad/banner_ad.html
    	modified:   src/main/resources/templates/ad/top_ad.html
    	modified:   src/main/resources/templates/bofang.html
    	new file:   src/main/resources/templates/footer.html
    	modified:   src/main/resources/templates/header.html
    	modified:   src/main/resources/templates/list.html
    	modified:   src/main/resources/templates/mobile/bofang.html
    	new file:   src/main/resources/templates/mobile/footer.html
    	modified:   src/main/resources/templates/mobile/listwap.html
    	modified:   src/main/resources/templates/mobile/playing.html
    	modified:   src/main/resources/templates/playing_pc.html

--- src/main/java/com/example/cms9cc/template/PlayController.java
+++ src/main/java/com/example/cms9cc/template/PlayController.java
@@ -1,6 +1,10 @@
 package com.example.cms9cc.template;
 
+import com.example.cms9cc.admin.AdminService;
+import com.example.cms9cc.template.bean.PlayInfoBean;
+import com.example.cms9cc.template.service.PlayService;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,13 +15,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/live")
 public class PlayController {
 
-    @GetMapping("/basketball/{id}")
-    public String playBasketBall(HttpServletRequest request, Model model, @PathVariable("id") Integer id){
-        model.addAttribute("requestUrl", request.getRequestURI());
-        return "bofang";
+    AdminService adminService;
+    PlayService playService;
+    @Autowired
+    public PlayController(AdminService adminService,PlayService playService) {
+        this.adminService = adminService;
+        this.playService = playService;
     }
 
-    public String playFootBall(@PathVariable Integer id){
+    @GetMapping("/{id}")
+    public String playBasketBall(HttpServletRequest request, Model model, @PathVariable("id") Long id){
+        model.addAttribute("requestUrl", request.getRequestURI());
+        getBaseModel(model);
+        PlayInfoBean liveInfo = playService.getLiveInfo(id);
+        model.addAttribute("live_info",liveInfo);
         return "bofang";
+    }
+    public Model getBaseModel(Model model) {
+        model.addAttribute("config", adminService.getAllConfig());
+        return model;
     }
 }
